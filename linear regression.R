69574596aab51935c59719cd3bf941653e7f6b50
nununu-py <09021381621081@student.unsri.ac.id>
2023-02-10 21:42:38 +0700

    add feature scaling in linear regression

--- linear regression.R
+++ linear regression.R
@@ -1,10 +1,10 @@
 dataframe <- read.csv("E:\\Data Science\\R\\it box\\data\\Salary_Data.csv")
 
-dataframe$User.ID <- NULL
-dataframe$Purchased <- NULL
-
 library(ggplot2)
 ggplot(data = dataframe, aes(YearsExperience, Salary)) + geom_point()
+
+# feature scaling
+dataframe[, 1] <- scale(dataframe[, 1])
 
 library(caTools)
 
@@ -27,3 +27,12 @@ regressor <- lm(
 y_pred <- predict(regressor, newdata = test_data)
 
 result <- cbind(test_data, y_pred)
+
+summary(result$Salary)
+summary(result$y_pred)
+
+salary_dist <- ggplot(data = result, aes(Salary)) + geom_boxplot()
+salary_dist
+
+salarypredict_dist <- ggplot(data = result, aes(y_pred)) + geom_boxplot()
+salarypredict_dist
